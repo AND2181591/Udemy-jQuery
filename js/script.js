cc00d6289577433b6100aeb7a9dcb349d9e42a16
Andrew760 <andrew.camacho760@yahoo.com>
2021-08-25 15:40:38 -0700

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function() {
+  // jQuery goes here...
+
+  // Uncomment this line to fade out the red box on page load
+  $(".red-box").fadeTo(1000, 0.2);
+  $(".green-box").fadeTo(1000, 0.5);
+  $(".blue-box").fadeTo(1000, 0.8);
+
+  $(".green-box").fadeToggle();
+  $(".green-box").fadeToggle();
+});
+
+
