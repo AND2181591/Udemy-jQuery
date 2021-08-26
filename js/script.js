bd6b59b4ee2e119c39fa7f5f3141bf172cf3b010
Andrew760 <andrew.camacho760@yahoo.com>
2021-08-25 17:01:16 -0700

    Section Two completed

--- js/script.js
+++ js/script.js
@@ -2,12 +2,25 @@ $(function() {
   // jQuery goes here...
 
   // Uncomment this line to fade out the red box on page load
-  $(".red-box").fadeTo(1000, 0.2);
-  $(".green-box").fadeTo(1000, 0.5);
-  $(".blue-box").fadeTo(1000, 0.8);
+  // $(".blue-box").slideUp(2000).slideDown(1000);
 
-  $(".green-box").fadeToggle();
-  $(".green-box").fadeToggle();
+  // $("p").hide().slideDown(1000).slideToggle(500);
+
+  $(".blue-box")
+  .animate({
+    "margin-left": "+=200px"
+  }, 1000, "linear")
+  .animate({
+    "margin-left": "-=200px"
+  }, 500);
+
+  $("p").animate({
+    fontSize: "50px"
+  }, 1000, message);
+
+  function message() {
+    alert("The paragraphs are huge!");
+  }
 });
 
 
